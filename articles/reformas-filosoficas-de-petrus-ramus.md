b6e4149aba5d59c4e0963acb394b03f8420eb62e
Heleno Salgado <helenosalgado19@gmail.com>
2026-06-21 02:28:08 -0300

    Adicionando novo artigo

--- articles/reformas-filosoficas-de-petrus-ramus.md
+++ articles/reformas-filosoficas-de-petrus-ramus.md
@@ -1,5 +1,9 @@
 ---
 title: "REFORMAS FILOSÓFICAS DE PETRUS RAMUS: A SIMPLIFICAÇÃO DA LÓGICA E A SISTEMATIZAÇÃO DA METODOLOGIA"
+journal: History of Science
+volume: 6
+issue: 5
+pages: 101-112
 authors:
   - name: "Djamila Abdullazade"
     orcid: "0009-0007-5639-8512"
@@ -17,13 +21,17 @@ BBK: 87.3:87.4
 HoS: 113
 DOI: 10.33864/2790-0037.2025.v6.i5.101-112  
 
-categories: 
+keywords: 
   - Renascimento  
   - Humanismo
   - Lógica
   - Lógica aristotélica
   - Metodização
   - Petrus Ramus
+
+categories: 
+  - Filosofia
+  - Lógica
 
 license: "Direitos autorais: © 2025 por AcademyGate Publishing. Este artigo é um artigo de acesso aberto distribuído sob os termos e condições da licença CC BY-NC 4.0. Para obter detalhes sobre esta licença, visite https://creativecommons.org/licenses/by-nc/4.0/."
 ---
---
+++ scripts/converte-pdf-in-markdown.js
@@ -0,0 +1,37 @@
+---
+title: "REFORMAS FILOSÓFICAS DE PETRUS RAMUS: A SIMPLIFICAÇÃO DA LÓGICA E A SISTEMATIZAÇÃO DA METODOLOGIA"
+journal: History of Science
+volume: 6
+issue: 5
+pages: 101-112
+authors:
+  - name: "Djamila Abdullazade"
+    orcid: "0009-0007-5639-8512"
+    email: "jamila.abdullazadee@gmail.com"
+    affiliation: "Universidade Estatal de Baku"
+  - name: "Aladdin Malikov"
+    orcid: "0000-0001-5830-6764"
+    email: "aladdin.malikov@gmail.com"
+    affiliation: "AcademyGate Publishing"
+summary: "O artigo analisa as concepções filosóficas e reformistas de Petrus Ramus, representante da época do Renascimento. Como um dos mais destacados reformadores do Renascimento, ele deixou um profundo legado. O artigo assinala que Ramus, graças às reformas que realizou tanto na pedagogia quanto na filosofia, exerceu significativa influência sobre o desenvolvimento do pensamento científico."
+date: 20-06-2026
+
+UDC: 1(091):161/162  
+BBK: 87.3:87.4  
+HoS: 113
+DOI: 10.33864/2790-0037.2025.v6.i5.101-112  
+
+keywords: 
+  - Renascimento  
+  - Humanismo
+  - Lógica
+  - Lógica aristotélica
+  - Metodização
+  - Petrus Ramus
+
+categories: 
+  - Filosofia
+  - Lógica
+
+license: "Direitos autorais: © 2025 por AcademyGate Publishing. Este artigo é um artigo de acesso aberto distribuído sob os termos e condições da licença CC BY-NC 4.0. Para obter detalhes sobre esta licença, visite https://creativecommons.org/licenses/by-nc/4.0/."
+---
